4b337228e135013270b746919ff3b16a9694ddb6
benflips <benflips@users.noreply.github.com>
2020-09-15 16:32:50 +0800

    Add K1, K2 and isoPoints function

--- LVFunctions.R
+++ LVFunctions.R
@@ -25,11 +25,11 @@ library(deSolve)
 ## ---------------------------
 
 # The L-V system
-  # pars are list r1, r2, a12, a21, K
+  # pars are list r1, r2, a12, a21, K1, K2
 LVComp <- function(t, N, pars){
   list(c(
-    pars$r1*N[1]*(1-(N[1]+pars$a12*N[2])/pars$K),
-    pars$r2*N[2]*(1-(N[2]+pars$a21*N[1])/pars$K)
+    pars$r1*N[1]*(1-(N[1]+pars$a12*N[2])/pars$K1),
+    pars$r2*N[2]*(1-(N[2]+pars$a21*N[1])/pars$K2)
     )
   )
 }
@@ -40,5 +40,13 @@ LVComp <- function(t, N, pars){
   # # pars are list r1, r2, a12, a21, K
 LVSolve <- function(N0, maxTime = 100, pars){
   as.data.frame(ode(y = N0, times = 0:maxTime, func = LVComp, parms = pars))
+}
+
+# Get critical points for isoclines given parameters
+isoPoints <- function(pars){
+  iso12x <- c(0, pars$K1, 0, pars$K2/pars$a21)
+  iso12y <- c(pars$K1/pars$a12, 0, pars$K2, 0)
+  isoID <- c(1, 1, 2, 2)
+  data.frame(isoID, iso12x, iso12y)
 }
 
