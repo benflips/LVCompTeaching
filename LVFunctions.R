80ac1499f79f17cdf0c1f6cc75fb79253f58af49
benflips <benflips@users.noreply.github.com>
2020-09-15 09:18:57 +0800

    Add LV function and solver

---
+++ LVFunctions.R
@@ -0,0 +1,44 @@
+## ---------------------------
+##
+## Script name: LVFunctions.R
+##
+## Purpose of script: Functions for running and plotting numerical solutions to LV competition equations
+##
+## Author: Ben Phillips
+##
+## Date Created: 2020-09-15
+##
+## Email: phillipsb@unimelb.edu.au
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## --------------------------
+## load up the packages we will need 
+library(deSolve)
+## ---------------------------
+
+## load up our functions into memory
+
+## ---------------------------
+
+# The L-V system
+  # pars are list r1, r2, a12, a21, K?ode
+LVComp <- function(t, N, pars){
+  list(c(
+    pars$r1*N[1]*(1-(N[1]+pars$a12*N[2])/pars$K),
+    pars$r2*N[2]*(1-(N[2]+pars$a21*N[1])/pars$K)
+    )
+  )
+}
+
+# Solve it
+  # N0 is a vector of initial N1, N2
+  # maxTime is maximum number of generations
+  # # pars are list r1, r2, a12, a21, K
+LVSolve <- function(N0, maxTime = 100, pars){
+  ode(N0, 0:maxTime, LVComp, pars)
+}
+
